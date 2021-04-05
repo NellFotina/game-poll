bb9947b87b8fa1544f78396604dabc0185b1bd1b
NellFotina <fotina.nel@gmail.com>
2021-04-05 14:58:37 +0300

    main + result + pages + range

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,14 @@
-function show(state) {
-  document.getElementById("register").style.display = state;
-  document.getElementById("gray").style.display = state;
+function GetIntRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function onClickSpin() {
+    document.getElementById('buttons-animate').classList.toggle('animated');
+    setTimeout(function () {
+        handler()
+    }, 1000);
+}
+
+function handler() {
+    location.href = "pages/" + "page" + GetIntRandom(1, 16) + ".html";
 }
