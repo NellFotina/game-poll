3c1875611140666cf193f344221652fbf5b859db
NellFotina <fotina.nel@gmail.com>
2021-05-24 20:26:05 +0300

    Pause + всплывающие подсказки

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,39 @@
-// import { register, signIn } from "./users.js";
-import "./questions.js";
+import { verifyAuth, register, signIn } from "./users.js";
+import * as handler from "./questions.js";
 
 const registerForm = document.getElementById("signUpForm");
 const signInForm = document.getElementById("signInForm");
+const signInOutButton = document.getElementById("signInOutButton");
+const btnStart = document.querySelector(".btn-start");
 
-// registerForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   const formArray = Array.from(event.target);
-//   let userData = {};
-//   formArray.forEach((value) => {
-//     userData[value.id] = value.value;
-//   });
-//   register(userData);
-// });
+btnStart.addEventListener("click", onClickSpin);
 
-// signInForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   const formArray = Array.from(event.target);
-//   let userData = {};
-//   formArray.forEach((value) => {
-//     userData[value.id] = value.value;
-//   });
-//   signIn(userData);
-// });
+function onClickSpin() {
+  document.getElementById("buttons-animate").classList.toggle("animated");
+  setTimeout(function () {
+    location.href = "pages/question.html";
+    handler();
+  }, 1000);
+}
 
-// questions();
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formArray = Array.from(event.target);
+  let userData = {};
+  formArray.forEach((value) => {
+    userData[value.id] = value.value;
+  });
+  register(userData);
+});
+
+signInForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formArray = Array.from(event.target);
+  let userData = {};
+  formArray.forEach((value) => {
+    userData[value.name] = value.value;
+  });
+  signIn(userData);
+});
+
+verifyAuth();
--- js/users.js
+++ js/users.js
@@ -1,54 +1,79 @@
-const url = 'https://pollgame-be.herokuapp.com/'
+const url = "https://pollgame-be.herokuapp.com/";
+
+const statusText = document.getElementById("statusText");
+const status2Text = document.getElementById("status2Text");
+const modalSignIn = document.getElementById("modalSignIn");
+const signInOutButton = document.getElementById("signInOutButton");
+const userInfo = document.getElementById("userInfo");
 
 async function apiPost(url, userData) {
   const requestProp = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Mode': 'NOCORS',
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Mode: "NOCORS",
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(userData)
+    body: JSON.stringify(userData),
   };
 
   const response = await fetch(url, requestProp);
-    
+
   if (!response.ok) {
     const error = response.status + " " + response.statusText;
-    throw new Error(error)
-    }
+    throw new Error(error);
+  }
   const body = await response.json();
-  return body
+  return body;
 }
 
-export async function register (userData) {
-  console.log('Выполняется регистрация...');
+export async function register(userData) {
+  status2Text.innerText = "Виконується реєстрація ...";
   const fullUrl = `${url}users`;
   console.log(userData);
 
-  apiPost(fullUrl, userData).then(( responseData ) => {
-    console.log("Регистрация выполнена");
-    console.log(responseData);
-  })
-  .catch(error => {
-    console.log(error.message)
-    if (error.message === "417 Expectation Failed")  {
-      console.log("Такой email уже зарегистрирован");
-    } else {
-      console.log("Ошибка регистрации");
-    }
-  })
+  apiPost(fullUrl, userData)
+    .then((responseData) => {
+      status2Text.innerText = "Реєстрація виконана";
+      $("#modalSignUp").modal("hide");
+      signIn(userData);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      if (error.message === "417 Expectation Failed") {
+        status2Text.innerText = "Такий email або телефон вже зареєстрований";
+      } else {
+        status2Text.innerText = "Помилка реєстрації";
+      }
+    });
+}
+
+const setSignIn = () => {
+  localStorage.clear();
+  userInfo.innerText = "";
+  signInOutButton.innerText = "Вхiд";
+  signInOutButton.dataset.bsToggle = "modal";
+  signInOutButton.dataset.bsTarget = "#modalSignIn";
 };
 
-export async function signIn (userData) {
+const setSignOut = () => {
+  signInOutButton.innerText = "Вихiд";
+  signInOutButton.addEventListener("click", setSignIn);
+  delete signInOutButton.dataset.bsToggle;
+  delete signInOutButton.dataset.bsTarget;
+};
+
+export async function signIn(userData) {
   const fullUrl = url + "signin";
 
-  apiPost(fullUrl, userData).then(( responseData ) => {
-      console.log("Вход выполнен");
-      console.log(responseData);
-      const UserData = {phone: "", nickname: "", fullname: ""};
-      if(responseData) {
-        console.log(responseData);
+  apiPost(fullUrl, userData)
+    .then((responseData) => {
+      if (statusText) statusText.innerText = "Вхід виконано";
+      userInfo.innerText = "Вітаємо " + responseData.email;
+      setTimeout(() => $("#modalSignIn").modal("hide"), 1000);
+      setSignOut();
+
+      if (responseData) {
         localStorage.setItem("token", responseData.token);
         localStorage.setItem("refreshToken", responseData.refreshToken);
         localStorage.setItem("userId", responseData.userId);
@@ -61,11 +86,38 @@ export async function signIn (userData) {
     .catch((error) => {
       console.log(error.message);
       if (error.message === "403 Forbidden") {
-        console.log("Неверный пароль");
+        statusText.innerText = "Невірний пароль";
       } else if (error.message === "404 Not Found") {
-        console.log("Пользователь с таким email не найден");
+        statusText.innerText = "Користувач з таким email не найден";
       } else if (error.message) {
-        console.log("Ошибка входа");
+        statusText.innerText = "Помилка входу";
       }
     });
-};
+}
+
+export async function verifyAuth() {
+  const bearerToken = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+  const requestProp = {
+    method: "GET",
+    headers: {
+      Mode: "NOCORS",
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + bearerToken,
+    },
+  };
+  const fullUrl = url + "users/" + userId;
+
+  const res = await fetch(fullUrl, requestProp);
+
+  if (!res.ok) {
+    const error = res.status + " " + res.statusText;
+    setSignIn();
+    throw new Error(error);
+  } else {
+    const body = await res.json();
+    setSignOut();
+    userInfo.innerText = "Вітаємо " + body.email;
+  }
+}
